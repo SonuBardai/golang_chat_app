dc6f9db1850acb9b65c706a551d100119d7dbc54
SonuBardai <sonubardai9@gmail.com>
2023-07-17 08:56:03 +0530

    Create structs for Users and Server

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,9 @@ module github.com/SonuBardai/chat_app/server
 
 go 1.20
 
-require golang.org/x/net v0.12.0 // indirect
+require (
+	github.com/go-faker/faker/v4 v4.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -4,38 +4,76 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-faker/faker/v4"
+	"github.com/google/uuid"
 	"golang.org/x/net/websocket"
 )
 
-var users []*websocket.Conn
-
-func SayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+type User struct {
+	userId   int
+	userName string
+	conn     *websocket.Conn
 }
 
-func WSHandler(ws *websocket.Conn) {
+func newUser(ws *websocket.Conn, userName string) User {
+	user := User{userId: int(uuid.New().ID()), userName: userName, conn: ws}
+	return user
+}
+
+type Users struct {
+	conns []User
+}
+
+func (u *Users) addUser(ws *websocket.Conn, userName string) {
+	u.conns = append(u.conns, newUser(ws, userName))
+}
+
+func (u *Users) removeUser(d *User) {
+	for i, user := range u.conns {
+		if user.conn == d.conn {
+			u.conns = append(u.conns[:i], u.conns[i+1:]...)
+		}
+	}
+}
+
+func (u *Users) broadcast(message string) {
+	fmt.Println("Broadcasting to Users: ", u.conns)
+	for _, user := range u.conns {
+		if err := websocket.Message.Send(user.conn, message); err != nil {
+			fmt.Println("error sending message")
+			u.removeUser(&user)
+		}
+	}
+}
+
+type WsServer struct {
+	conn  *websocket.Conn
+	users *Users
+}
+
+func (s *WsServer) readLoop() {
 	var message string
-	users = append(users, ws)
 	for {
-		err := websocket.Message.Receive(ws, &message)
+		err := websocket.Message.Receive(s.conn, &message)
 		if err != nil {
 			fmt.Println("error receiving message")
 			return
 		}
 		fmt.Println("Received from socket: ", message)
-		for _, user := range users {
-			err = websocket.Message.Send(user, message)
-			if err != nil {
-				fmt.Println("error sending message")
-				return
-			}
-		}
+		s.users.broadcast(message)
 	}
+}
+
+var globalUsers = Users{conns: make([]User, 0)}
+
+func WSHandler(ws *websocket.Conn) {
+	wsServer := WsServer{conn: ws, users: &globalUsers}
+	globalUsers.addUser(ws, faker.FirstName())
+	wsServer.readLoop()
 }
 
 func main() {
 	handler := http.NewServeMux()
-	handler.HandleFunc("/", SayHello)
 	handler.Handle("/ws", websocket.Handler(WSHandler))
 	fmt.Println("Listening on port 3001")
 	http.ListenAndServe(":3001", handler)
